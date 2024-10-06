216453196670c32b19bacf26e419c4e83b7ab690
lin-dongyizhi7 <2985956026@qq.com>
2024-10-06 16:39:23 +0800

    wip: 初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Slider></Slider>
+    <Recommend></Recommend>
+  </div>
+</template>
+
+<script setup>
+import Slider from '../components/home/Slider'
+import Recommend from '../components/home/Recommend';
+ 
+</script>
+
+<style scoped lang="less">
+</style>
