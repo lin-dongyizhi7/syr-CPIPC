c1967986df4a0e7db544c8b7dad7669f3817737b
lin-dongyizhi7 <2985956026@qq.com>
2024-10-15 23:00:51 +0800

    wip:vue初始化

---
+++ DISFR-web/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home-page">
+        
+    </div>
+</template>
+
+<script setup>
+import IndexStruct from './indexStruct/index.vue';
+import TrainPage from './train/index.vue';
+import Predict from './predict/index.vue';
+ 
+</script>
+
+<style scoped lang="less">
+</style>
